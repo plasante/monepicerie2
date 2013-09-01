b6cb65026759a9f4bce8a36b3f2b443aaef1e3e7
Pierre Lasante <plasante@videotron.ca>
2013-09-01 11:21:35 -0400

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Monepicerie2::Application.config.secret_key_base = 'f8f3aeb1dd5c24f116d162e391a0012b537d2dbf976419588f99055f25e17c175026010dc8200a4da916747d31d63bb8b27690e2f043ed7d13aa84d9327781d0'
