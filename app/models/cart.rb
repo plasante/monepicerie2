872da56e0ad0889b25c3dce0c0e15a2c2a9b1744
Pierre Lasante <plasante@videotron.ca>
2013-09-05 18:01:29 -0400

    Il y a un probleme lorsque je sauvegarde une commande. Les line_items ne sont pas sauvegarde.

--- app/models/cart.rb
+++ app/models/cart.rb
@@ -16,4 +16,11 @@ class Cart < ActiveRecord::Base
 	def total_price
   		line_items.to_a.sum { |item| item.total_price }
 	end
+
+	def add_line_items_from_cart(cart)
+    	cart.line_items.each do |item|
+      		item.cart_id = nil
+      		line_items << item
+      	end
+    end
 end
