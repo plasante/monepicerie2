2b05add84c9bf95ef2b7a3c4434cb972d3caf9f6
Pierre Lasante <plasante@videotron.ca>
2013-09-07 11:36:58 -0400

    reached chapter 13

--- config/routes.rb
+++ config/routes.rb
@@ -9,6 +9,10 @@ Monepicerie2::Application.routes.draw do
   resources :products
 
   root 'store#index', as: 'store'
+
+  resources :products do
+    get :who_bought, on: :member
+  end
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
